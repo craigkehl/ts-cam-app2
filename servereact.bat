90d64716352d273b2fce70c30e2e208ab37fb2d2
ckehl <codechck@gmail.com>
2023-01-08 15:24:12 -0700

    batch location

--- servereact.bat
+++ servereact.bat
@@ -1,4 +1,4 @@
 if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
 @echo off
 title react
-call serve C:\Repo\churchBeta\React\ts-cam-app\build
+call serve C:\Repo\church-2023\ts-cam-app2\build
